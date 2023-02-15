db356f577a568f9dcda27c53bd92e77c6af33dcb
keven <kevengrillo@gmail.com>
2023-02-15 18:31:20 -0300

    Add project files.

---
+++ AppMvc/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMvc.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Nome completo")]
+        [Required(ErrorMessage = " O campo {0} é requirido ")]
+        public string Nome { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = " O campo {0} é requirido ")]
+        [EmailAddress(ErrorMessage = "E-mail formato errado")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = " O campo {0} é requirido ")]
+        public string CPF { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataMatricula { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
